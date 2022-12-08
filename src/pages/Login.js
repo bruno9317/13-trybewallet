d755fe05be4fc4b822dfab02245b141b6083ef51
bruno <brunobaptista9317@gmail.com>
2022-12-08 18:22:01 -0300

    salvando requisito 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,76 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addUser } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    senha: '',
+    isButtonDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      const { email, senha } = this.state;
+      const tamSenhaMinima = 6;
+      const newButtonState = email.includes('@')
+      && email.includes('.com')
+      && senha.length >= tamSenhaMinima;
+      this.setState({ isButtonDisabled: !newButtonState });
+    });
+  };
+
+  handleClick = () => {
+    const { dispatch, history } = this.props;
+    dispatch(addUser(this.state));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Loginn</div>;
+    const { email, senha, isButtonDisabled } = this.state;
+    return (
+      <form>
+        <label htmlFor="email">
+          Email
+          <input
+            name="email"
+            id="email"
+            type="text"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="senha">
+          Senha
+          <input
+            name="senha"
+            id="senha"
+            type="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            value={ senha }
+          />
+        </label>
+        <button
+          type="button"
+          onClick={ this.handleClick }
+          disabled={ isButtonDisabled }
+        >
+          Enviar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,12 @@
 // Coloque aqui suas actions
+export const ADD_USER_INFO = 'ADD_USER_INFO';
+
+export const addUser = (userInfo) => {
+  console.log('chamou action');
+  return {
+    type: ADD_USER_INFO,
+    payload: {
+      ...userInfo,
+    },
+  };
+};
